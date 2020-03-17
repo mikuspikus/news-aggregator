c87344ecd7b87457ebb0896b8300c71f49801ef2
Yurii Kalugin <pyro74rus@gmail.com>
2020-03-17 15:59:02 +0300

    Moved generic part of requester to its own module

---
+++ auth/api/base/views.py
@@ -0,0 +1,19 @@
+from logging import getLogger
+
+class BaseView(APIView):
+    logger = getLogger('views')
+    formatter = '{method} : {url} : {content_type} : {msg}'
+
+    def __format(self, request: Request, msg: str = None) -> str:
+        return self.formatter.format(
+                method = request.method,
+                url = request.get_raw_uri(),
+                content_type = request.content_type,
+                msg = msg
+            )
+
+    def info(self, request: Request, msg: str = None) -> None:
+        self.logger.info(self.__format(request, msg))
+
+    def exception(self, request: Request, msg: str = None) -> None:
+        self.logger.exception(self.__format(request, msg))
