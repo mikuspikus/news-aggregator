1d6ca2cb91c1b8509228d7852c0a10f245c87348
Yurii Kalugin <pyro74rus@gmail.com>
2020-03-21 13:28:04 +0300

    Added admin site models edition

--- auth/api/admin.py
+++ auth/api/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import AuthUser, AuthToken, UsersToken, NewsToken, CommentsToken, RSSParserToken, StatsToken
+
+
+admin.site.register(AuthUser, UserAdmin)
+
+class AuthTokenAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(AuthToken, AuthTokenAdmin)
+
+class UsersTokenAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(UsersToken, UsersTokenAdmin)
+
+class NewsTokenAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(NewsToken, NewsTokenAdmin)
+
+class CommentsTokenAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(CommentsToken, CommentsTokenAdmin)
+
+class RSSParserTokenAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(RSSParserToken, RSSParserTokenAdmin)
+
+class StatsTokenAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(StatsToken, StatsTokenAdmin)
