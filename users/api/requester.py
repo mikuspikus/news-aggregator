ffdd6814c7712c1d2fac1b24001c61cff537ffbf
Yurii Kalugin <pyro74rus@gmail.com>
2020-03-22 19:57:22 +0300

    Added remote user handling

--- users/api/requester.py
+++ users/api/requester.py
@@ -5,20 +5,47 @@ from requests.exceptions import RequestException
 
 from typing import Tuple
 
+from django.conf import settings
+
+ERRORS_FIELD = settings.ERRORS_FIELD
+
 class UsersRequester(BaseRequester):
     URLS = {}
 
-    @staticmethod
     def authenticate(self, token: str) -> Tuple[dict, int]:
         try:
             response = self.get(
-                url = self.URLS['auth'],
+                url = self.URLS['authenticate'],
                 headers = {'Authentication': token}
             )
 
         except RequestException as error:
-            msg = str(error)
-            self.exception(msg)
-            raise ConnectionError(msg)
+            return self.process_error(error)
+
+        return response.json(), response.status_code
+
+    def send_credentials(self, credentials: dict) -> Tuple[dict, int]:
+        try:
+            response = self.post(
+                url = self.URLS['send-credentials'],
+                data = credentials,
+                headers = None # add headers
+            )
+
+        except RequestException as error:
+            return self.process_error(error)
+
+        return response.json(), response.status_code
+
+    def update_credentials(self, credentials: dict) -> Tuple[dict, int]:
+        try:
+            response = self.patch(
+                url = self.URLS['update-credentials'],
+                data = credentials,
+                headers = None # add headers
+            )
+
+        except RequestException as error:
+            return self.process_error(error)
 
         return response.json(), response.status_code
