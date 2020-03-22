45316225073481185e08c6f256d924716bba2405
Yurii Kalugin <pyro74rus@gmail.com>
2020-03-22 19:56:47 +0300

    Changed user model

--- users/api/models.py
+++ users/api/models.py
@@ -2,5 +2,5 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from uuid import uuid4
 
-class CustomUser(AbstractUser):
-    uuid = models.UUIDField(unique = True, default = uuid4, editable = False)
+class User(AbstractUser):
+    id = models.UUIDField(primary_key = True, default = uuid4, editable = False)
