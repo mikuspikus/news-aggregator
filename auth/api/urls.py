b770e1de5c076916679f9fa09550de35ea6fd9c0
Yurii Kalugin <pyro74rus@gmail.com>
2020-03-22 18:02:46 +0300

    Updated urls

--- auth/api/urls.py
+++ auth/api/urls.py
@@ -1,17 +1,30 @@
 from django.urls import path
 
-from .views import UserLoginView, AuthTokenView, UsersTokenView, CommentsTokenView, RSSParserTokenView, NewsTokenView, StatsTokenView
+from .views.servicetoservice import (
+    AuthServiceTokenView,
+    UsersServiceTokenView,
+    CommentsServiceTokenView,
+    RSSParserServiceTokenView,
+    NewsServiceTokenView,
+    StatsServiceTokenView,
+)
+from .views.login import UserLoginView, AuthTokenView
 
-TOKENS = 'tokens'
+TOKENS = "tokens"
 
 urlpatterns = [
-    path(f'{TOKENS}/users', UsersTokenView.as_view(), name = 'users-tokens'),
-    path(f'{TOKENS}/comments', CommentsTokenView.as_view(), name = 'comments-tokens'),
-    path(f'{TOKENS}/rss-parser', RSSParserTokenView.as_view(), name = 'rss-parser-tokens'),
-    path(f'{TOKENS}/news', NewsTokenView.as_view(), name = 'news-tokens'),
-    path(f'{TOKENS}/stats', StatsTokenView.as_view(), name = 'stats-tokens'),
-    path(f'{TOKENS}/auth', AuthTokenView.as_view(), name = 'auth-tokens'),
-
-    path(f'login', UserLoginView.as_view(), name = 'user-login'),
-
+    path(f"{TOKENS}/users", UsersServiceTokenView.as_view(), name="users-tokens"),
+    path(
+        f"{TOKENS}/comments", CommentsServiceTokenView.as_view(), name="comments-tokens"
+    ),
+    path(
+        f"{TOKENS}/rss-parser",
+        RSSParserServiceTokenView.as_view(),
+        name="rss-parser-tokens",
+    ),
+    path(f"{TOKENS}/news", NewsServiceTokenView.as_view(), name="news-tokens"),
+    path(f"{TOKENS}/stats", StatsServiceTokenView.as_view(), name="stats-tokens"),
+    path(f"{TOKENS}/auth", AuthServiceTokenView.as_view(), name="auth-tokens"),
+    path("logged-in", AuthTokenView.as_view(), name="user-logged-in"),
+    path("login", UserLoginView.as_view(), name="user-login"),
 ]
