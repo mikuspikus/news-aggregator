7a8160e8067bcb54788fe04c26d9d4a17ae424cd
Yurii Kalugin <pyro74rus@gmail.com>
2020-03-22 18:06:34 +0300

    Formatted with 'black'

--- auth/api/admin.py
+++ auth/api/admin.py
@@ -1,35 +1,65 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import AuthUser, AuthToken, AuthServiceToken, UsersServiceToken, NewsServiceToken, CommentsServiceToken, RSSParserServiceToken, StatsServiceToken
+from .models import (
+    AuthUser,
+    AuthToken,
+    AuthServiceToken,
+    UsersServiceToken,
+    NewsServiceToken,
+    CommentsServiceToken,
+    RSSParserServiceToken,
+    StatsServiceToken,
+)
 
 
 admin.site.register(AuthUser, UserAdmin)
 
+
 class AuthTokenAdmin(admin.ModelAdmin):
     pass
+
+
 admin.site.register(AuthToken, AuthTokenAdmin)
+
 
 class AuthServiceTokenAdmin(admin.ModelAdmin):
     pass
+
+
 admin.site.register(AuthServiceToken, AuthServiceTokenAdmin)
+
 
 class UsersServiceTokenAdmin(admin.ModelAdmin):
     pass
+
+
 admin.site.register(UsersServiceToken, UsersServiceTokenAdmin)
+
 
 class NewsServiceTokenAdmin(admin.ModelAdmin):
     pass
+
+
 admin.site.register(NewsServiceToken, NewsServiceTokenAdmin)
+
 
 class CommentsServiceTokenAdmin(admin.ModelAdmin):
     pass
+
+
 admin.site.register(CommentsServiceToken, CommentsServiceTokenAdmin)
+
 
 class RSSParserServiceTokenAdmin(admin.ModelAdmin):
     pass
+
+
 admin.site.register(RSSParserServiceToken, RSSParserServiceTokenAdmin)
+
 
 class StatsServiceTokenAdmin(admin.ModelAdmin):
     pass
+
+
 admin.site.register(StatsServiceToken, StatsServiceTokenAdmin)
