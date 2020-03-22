6dfd80f18c6bad82947fc5828af01d427c0b8a14
Yurii Kalugin <pyro74rus@gmail.com>
2020-03-22 18:02:00 +0300

    Added AUTh service serializer and updated other's names

--- auth/api/serializers.py
+++ auth/api/serializers.py
@@ -1,26 +1,30 @@
-from .models import UsersToken, RSSParserToken, CommentsToken, NewsToken, StatsToken
+from .models import AuthServiceToken, UsersServiceToken, RSSParserServiceToken, CommentsServiceToken, NewsServiceToken, StatsServiceToken
 from .tokenauthentication.serializers import TokenSerializer
 
 from rest_framework import serializers
 
 from django.conf import settings
 
-class UsersTokenSerializer(TokenSerializer):
+class AuthServiceTokenSerializer(TokenSerializer):
+    ID = settings.CREDENTIALS['AUTH']['ID']
+    SECRET = settings.CREDENTIALS['AUTH']['SECRET']
+
+class UsersServiceTokenSerializer(TokenSerializer):
     ID = settings.CREDENTIALS['USERS']['ID']
     SECRET = settings.CREDENTIALS['USERS']['SECRET']
 
-class CommentsTokenSerializer(TokenSerializer):
+class CommentsServiceTokenSerializer(TokenSerializer):
     ID = settings.CREDENTIALS['COMMENTS']['ID']
     SECRET = settings.CREDENTIALS['COMMENTS']['SECRET']
 
-class RSSPareserTokenSerializer(TokenSerializer):
+class RSSPareserServiceTokenSerializer(TokenSerializer):
     ID = settings.CREDENTIALS['RSS-PARSER']['ID']
     SECRET = settings.CREDENTIALS['RSS-PARSER']['SECRET']
 
-class NewsTokenSerializer(TokenSerializer):
+class NewsTokenServiceSerializer(TokenSerializer):
     ID = settings.CREDENTIALS['NEWS']['ID']
     SECRET = settings.CREDENTIALS['NEWS']['SECRET']
 
-class StatsTokenSerializer(TokenSerializer):
+class StatsTokenServiceSerializer(TokenSerializer):
     ID = settings.CREDENTIALS['STATS']['ID']
     SECRET = settings.CREDENTIALS['STATS']['SECRET']
