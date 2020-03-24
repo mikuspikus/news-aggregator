02c37f78a4bf3911c7c36dd8e10716801e5f4d6c
Yurii Kalugin <pyro74rus@gmail.com>
2020-03-24 22:39:10 +0500

    moved to generic and remoteauth modules

--- users/api/views.py
+++ users/api/views.py
@@ -3,112 +3,80 @@ from django.shortcuts import render
 from .models import User
 from .serializers import UserSerializer
 from .authentication import RemoteTokenAuthentication
-from .permissions import IsRemoteAuthenticated
 
 from rest_framework.views import APIView, Request, Response
 import rest_framework.status as st
 
 from uuid import UUID
 
-from logging import getLogger
+from remoteauth.permissions import IsRemoteAuthenticated
+from .permissions import IsAuthorizedAndUser
+from generic.views import BaseView
 
-class BaseView(APIView):
-    authentication_classes = (RemoteTokenAuthentication, )
-    permission_classes = []
-    logger = getLogger('views')
-    formatter = '{method} : {url} : {content_type} : {msg}'
-
-    def __format(self, request: Request, msg: str = None) -> str:
-        return self.formatter.format(
-                method = request.method,
-                url = request.get_raw_uri(),
-                content_type = request.content_type,
-                msg = msg
-            )
-
-    def info(self, request: Request, msg: str = None) -> None:
-        self.logger.info(self.__format(request, msg))
-
-    def exception(self, request: Request, msg: str = None) -> None:
-        self.logger.exception(self.__format(request, msg))
 
 class UserView(BaseView):
     model = User
     serializer = UserSerializer
-    permission_classes = (IsRemoteAuthenticated, )
+    permission_classes = (IsAuthorizedAndUser, )
 
     def get(self, request: Request, id_: UUID, format: str = 'json') -> Response:
         self.info(request, f'asked for object with id : {id_}')
 
-        try:
-            row_ = self.model.objects.get(pk = id_)
+        obj = self.get_object(request, id_)
+        serializer_ = self.serializer(instance=obj)
 
-        except self.model.DoesNotExist:
-            self.exception(f'object with id : {id_} not found')
-            return Response(status = st.HTTP_404_NOT_FOUND)
-
-        serializer_ = self.serializer(instance = row_)
-        return Response(data = serializer_.data, status = st.HTTP_200_OK)
+        return Response(data=serializer_.data, status=st.HTTP_200_OK)
 
     def patch(self, request: Request, id_: UUID, format: str = 'json') -> Response:
         self.info(request, f'asked to modify object with id : {id_}')
 
-        try:
-            row_ = self.model.objects.get(pk = id_)
-
-        except self.model.DoesNotExist as error:
-            self.exception(f'object with id : {id_} not found')
-            return Response(status = st.HTTP_404_NOT_FOUND)
-
-        serializer_ = self.serializer(instance = row_, data = request.data)
+        obj = self.get_object(request, id_)
+        serializer_ = self.serializer(instance=obj, data=request.data)
 
         if serializer_.is_valid():
             serializer_.save()
 
-            return Response(data = serializer_.data, status = st.HTTP_202_ACCEPTED)
+            return Response(data=serializer_.data, status=st.HTTP_202_ACCEPTED)
 
-        self.exception(request, f'not valid data for serializer : {serializer_.errors}')
-        return Response(data = serializer_.errors, status = st.HTTP_400_BAD_REQUEST)
+        self.exception(
+            request, f'not valid data for serializer : {serializer_.errors}')
+        return Response(data=serializer_.errors, status=st.HTTP_400_BAD_REQUEST)
 
     def delete(self, request: Request, id_: UUID, format: str = 'json') -> Response:
         self.info(request, f'asked to delete object with id : {id_}')
 
-        try:
-            row_ = self.model.objects.get(pk = id_)
+        obj = self.get_object(request, id_)
+        obj.delete()
 
-        except self.model.DoesNotExist:
-            self.exception(f'object with id : {id_} not found')
-            return Response(status = st.HTTP_404_NOT_FOUND)
+        return Response(status=st.HTTP_204_NO_CONTENT)
 
-        row_.delete()
-        return Response(status = st.HTTP_204_NO_CONTENT)
 
 class UsersView(BaseView):
     model = User
     serializer = UserSerializer
 
-    permission_classes = []
-    authentication_classes = []
-
+    permission_classes = [IsRemoteAuthenticated]
+    authentication_classes = [RemoteTokenAuthentication]
 
     def post(self, request: Request) -> Response:
         self.info(request, f'adding object')
 
-        serializer_ = self.serializer(data = request.data)
+        serializer_ = self.serializer(data=request.data)
 
         if serializer_.is_valid(raise_exception=True):
             serializer_.save()
 
-            return Response(data = serializer_.data, status = st.HTTP_201_CREATED)
+            return Response(data=serializer_.data, status=st.HTTP_201_CREATED)
 
-        self.exception(request, f'not valid data for serializer : {serializer_.errors}')
-        return Response(data = serializer_.errors, status = st.HTTP_400_BAD_REQUEST)
+        self.exception(
+            request, f'not valid data for serializer : {serializer_.errors}')
+        return Response(data=serializer_.errors, status=st.HTTP_400_BAD_REQUEST)
 
     def get(self, request: Request) -> Response:
         self.info(request, f'request objects')
 
         row_s_ = self.model.objects.all()
 
-        serializer_ = self.serializer(data = row_s_, many = True)
+        serializer_ = self.serializer(data=row_s_, many=True)
 
-        return Response(data = serializer_.data, status = st.HTTP_200_OK)
+        return Response(data=serializer_.data, status=st.HTTP_200_OK)
