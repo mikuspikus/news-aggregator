d72c3b8fa2d7765f1e6d283f8628933c68d292cf
Yurii Kalugin <pyro74rus@gmail.com>
2020-03-24 14:58:49 +0500

    Added URLs

---
+++ users/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import UserView, UsersView
+
+urlpatterns = [
+    path(f"users", UsersView.as_view(), name="users"),
+    path(f"users/<uuid:id_>", UserView.as_view(), name="user"),
+]
