267a57c312bcad64775ded52bb058604c28b0454
Yurii Kalugin <pyro74rus@gmail.com>
2020-03-24 22:38:04 +0500

    Added RemoteAuth module

---
+++ users/remoteauth/authentication.py
@@ -0,0 +1,20 @@
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.exceptions import NotAuthenticated, AuthenticationFailed
+import rest_framework.status as st
+
+from typing import Tuple
+
+
+class RemoteTokenAuthentication(TokenAuthentication):
+    keyword = 'Bearer'
+    authenticate = None
+
+    def authenticate_credentials(self, key: str) -> Tuple[None, str]:
+        data, code = self.authenticate(token=key)
+
+        if code != 200:
+            msg = data.get('error', 'Invalid or expired token')
+            raise AuthenticationFailed(detail=msg, code='authentication')
+        
+        data['token'] = key
+        return (None, data)
