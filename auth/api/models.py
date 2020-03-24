54778e178e60e9b8df3b0d452573b1a03e1ceac0
Yurii Kalugin <pyro74rus@gmail.com>
2020-03-24 22:41:16 +0500

    Added __str__

--- auth/api/models.py
+++ auth/api/models.py
@@ -3,13 +3,19 @@ from django.contrib.auth.models import AbstractUser
 
 from uuid import uuid4
 
-from .tokenauthentication.models import Token
+from tokenauthentication.models import Token
 
 class AuthUser(AbstractUser):
-    uuid = models.UUIDField(unique = True, default = uuid4, editable = False)
+    uuid = models.UUIDField(unique = True)
+
+    def __str__(self) -> str:
+        return f'{self.username} [{self.uuid}]'
 
 class UserAuthToken(Token):
     user = models.OneToOneField(to = AuthUser, on_delete = models.CASCADE)
+
+    def __str__(self) -> str:
+        return f'Token: <{self.token}:{self.created}> for {self.user}'
 
 class ServicesToken(Token):
     pass
