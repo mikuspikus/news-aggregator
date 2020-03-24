c443759f5e2d014ed2042ff0b6db0c33e0bf1d0a
Yurii Kalugin <pyro74rus@gmail.com>
2020-03-24 22:40:20 +0500

    Added generic modules

---
+++ auth/tokenauthentication/authentication.py
@@ -0,0 +1,52 @@
+from rest_framework.authentication import get_authorization_header, TokenAuthentication
+from rest_framework.exceptions import AuthenticationFailed, NotAuthenticated
+
+from typing import Tuple
+
+from .models import Token
+
+from datetime import timedelta, datetime
+from django.utils import timezone
+from django.conf import settings
+
+
+def expires_in(token: Token) -> datetime:
+    time_elapsed = timezone.now() - token.created
+    time_left = timedelta(settings.TOKEN_EXPIRED_AFTER_SECONDS) - time_elapsed
+
+    return time_left
+
+def is_token_expired(token: Token) -> bool:
+    return expires_in(token) < timedelta(seconds = 0)
+
+def token_handler(token: Token) -> bool:
+    is_expired = is_token_expired(token)
+
+    if is_expired:
+        token.delete()
+
+    return is_expired
+
+class GenericExpiringTokenAuthentication(TokenAuthentication):
+    '''Expiring token authentication based on original DRF TokenAuthentication
+    '''
+    model = None
+    keyword = 'Bearer'
+
+    def authenticate_credentials(self, key: str):
+        model = self.model
+
+        try:
+            token = model.objects.get(pk = key)
+
+        except model.DoesNotExist:
+            msg = 'Invalid token'
+            raise AuthenticationFailed(msg, code = 'authentication')
+
+        is_expired = token_handler(token)
+
+        if is_expired:
+            msg = 'Token has expired'
+            raise NotAuthenticated(msg, code = 'authentication')
+
+        return (None, token)
---
+++ auth/tokenauthentication/views.py
@@ -0,0 +1,27 @@
+from generic.views import BaseView
+from generic.permissions import IsAuthenticatedForMethods
+
+from rest_framework.views import Request, Response
+import rest_framework.status as st
+
+class TokenView(BaseView):
+    token_model = None
+    token_serializer = None
+    permission_classes = (IsAuthenticatedForMethods, )
+    authentication_classes = []
+
+    def get(self, request: Request, format: str = 'json') -> Response:
+        self.info(request, f'checking token {request.auth}')
+
+        return Response(status = st.HTTP_200_OK)
+
+
+    def post(self, request: Request, format: str = 'json') -> Response:
+        self.info(request, f'requesting token')
+
+        serializer = self.token_serializer(data = request.data, context = {'request': request})
+        serializer.is_valid(raise_exception = True)
+
+        token = self.token_model.objects.create()
+
+        return Response(data = {'token': token.token}, status = st.HTTP_200_OK)
