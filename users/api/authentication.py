584c87e3e7295ed775950c021f46c7e9111c7c6e
Yurii Kalugin <pyro74rus@gmail.com>
2020-03-24 22:37:19 +0500

    Removed useless imports

--- users/api/authentication.py
+++ users/api/authentication.py
@@ -1,9 +1,3 @@
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.exceptions import NotAuthenticated, AuthenticationFailed
-import rest_framework.status as st
-
-from typing import Tuple
-
 from remoteauth.authentication import RemoteTokenAuthentication
 from .requesters import authenticate
 
