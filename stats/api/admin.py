6ce26c9fe4af8a6bbd8f97ebbc9ea6248a038628
Yurii Kalugin <pyro74rus@gmail.com>
2020-04-08 23:33:47 +0500

    Added download .xlsx action to admin panel

--- stats/api/admin.py
+++ stats/api/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from django.http import HttpRequest, HttpResponse
+from  django.conf import settings
 
-# Register your models here.
+from .models import UsersStat, CommentsStat, RSSParserStat, NewsStat
+from .download import report
+
+
+FILENAME = getattr(settings, 'FILENAME', '{title}-{date}.xlsx')
+
+__default_headers = ('User', 'Datetime', 'Action', 'Input data', 'Output data')
+HEADERS = getattr(settings, 'HEADERS', __default_headers)
+
+TITLE = getattr(settings, 'TITLE', 'Stats')
+
+
+class UsersStatsAdmin(admin.ModelAdmin):
+    actions = ('download_report', )
+
+    def download_report(self, request: HttpRequest, queryset) -> HttpResponse:
+        return report(queryset, '[user]-stats', HEADERS, FILENAME)
+    download_report.short_description = "Download file for selected stats"
+
+admin.site.register(UsersStat, UsersStatsAdmin)
+
+
+class CommentsStatsAdmin(admin.ModelAdmin):
+    actions = ('download_report', )
+
+    def download_report(self, request: HttpRequest, queryset) -> HttpResponse:
+        return report(queryset, '[comments]-stats', HEADERS, FILENAME)
+    download_report.short_description = "Download file for selected stats"
+
+admin.site.register(CommentsStat, CommentsStatsAdmin)
+
+class RSSParserStatsAdmin(admin.ModelAdmin):
+    actions = ('download_report', )
+
+    def download_report(self, request: HttpRequest, queryset) -> HttpResponse:
+        return report(queryset, '[rss-parser]-stats', HEADERS, FILENAME)
+    download_report.short_description = "Download file for selected stats"
+
+admin.site.register(RSSParserStat, RSSParserStatsAdmin)
+
+class NewsStatsAdmin(admin.ModelAdmin):
+    actions = ('download_report', )
+
+    def download_report(self, request: HttpRequest, queryset) -> HttpResponse:
+        return report(queryset, '[news]-stats', HEADERS, FILENAME)
+    download_report.short_description = "Download file for selected stats"
+
+admin.site.register(NewsStat, NewsStatsAdmin)
---
+++ stats/api/download.py
@@ -0,0 +1,30 @@
+from django.http import HttpResponse
+
+import datetime as dt
+import openpyxl as xl
+from typing import List
+
+
+def report(queryset, title: str, headers: List[str], filename: str) -> HttpResponse:
+    response = HttpResponse(
+        content_type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    )
+    filename = filename.format(title = title, date = dt.datetime.now().strftime('%Y-%m-%d'))
+    response['Content-Disposition'] = f'attachment; filename="{filename}"'
+
+    workbook = xl.Workbook()
+    worksheet = workbook.active
+    worksheet.title = title
+
+    r_num = 1
+    for c_num, header in enumerate(headers):
+        worksheet.cell(row = r_num, column = c_num, value = header)
+
+    for row in queryset:
+        r_num += 1
+        
+        for c_num, value in enumerate(row, 1):
+            worksheet.cell(row = r_num, column = c_num, value = value)
+
+    workbook.save(response)
+    return response
