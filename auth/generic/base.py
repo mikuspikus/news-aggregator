e548769f441a9ed4dc07c80edc782fc4d4c92e61
Yurii Kalugin <pyro74rus@gmail.com>
2020-04-08 23:35:32 +0500

    Updated generic module

--- auth/generic/base.py
+++ auth/generic/base.py
@@ -12,37 +12,46 @@ FORMATTER = getattr(settings, 'FORMATTER', '')
 
 logger = getLogger(name='requesters')
 
+
 def __format(msg: str) -> str:
     return FORMATTER.format()
+
 
 def info(msg: str) -> None:
     logger.info(__format(msg))
 
+
 def exception(msg: str) -> None:
     logger.exception(__format(msg))
 
+
 def get(url: str, data: dict = {}, headers: dict = {}) -> requests.Response:
     info(f'requesting {url} with method \'GET\'')
-    return requests.get(url=url, data= data, headers = headers)
+    return requests.get(url=url, data=data, headers=headers)
+
 
 def post(url: str, data: dict = {}, headers: dict = {}) -> requests.Response:
     info(f'requesting {url} with method \'POST\'')
-    return requests.post(url=url, data= data, headers = headers)
+    return requests.post(url=url, data=data, headers=headers)
+
 
 def delete(url: str, headers: dict = {}) -> requests.Response:
     info(f'requesting {url} with method \'DELETE\'')
-    return requests.delete(url=url, headers= headers)
+    return requests.delete(url=url, headers=headers)
+
 
 def patch(url: str, data: dict = {}, headers: dict = {}) -> requests.Response:
     info(f'requesting {url} with method \'PATCH\'')
-    return requests.patch(url=url, data= data, headers = headers)
+    return requests.patch(url=url, data=data, headers=headers)
+
 
 def put(url: str, data: dict = {}, headers: dict = {}) -> requests.Response:
     info(f'requesting {url} with method \'PUT\'')
-    return requests.put(url=url, data= data, headers = headers)
+    return requests.put(url=url, data=data, headers=headers)
+
 
 def process_error(error: requests.exceptions.RequestException) -> Tuple[dict, int]:
     msg = str(error)
     exception(msg)
 
-    return ({ERRORS_FIELD: msg}, 503)
+    return ({ERRORS_FIELD: 'Connection to [auth] service failed'}, 503)
