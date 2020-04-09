501d0d449540fff08fb4e36716885940fd7d4304
Yurii Kalugin <pyro74rus@gmail.com>
2020-04-09 23:17:38 +0500

    Renamed file

--- stats/api/serializers.py
+++ stats/api/serializers.py
@@ -7,19 +7,23 @@ class NewsStatSerializer(GenericStatSerializer):
 
     class Meta:
         model = NewsStat
+        fields = '__all__'
 
 
 class CommentsStatSerializer(GenericStatSerializer):
 
     class Meta:
         model = CommentsStat
+        fields = '__all__'
 
 class RSSParserStatSerializer(GenericStatSerializer):
 
     class Meta:
         model = RSSParserStat
+        fields = '__all__'
 
 class UsersStatsSerializer(GenericStatSerializer):
 
     class Meta:
         model = UsersStat
+        fields = '__all__'
