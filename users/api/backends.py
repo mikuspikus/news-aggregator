350ad7a3cdd51c0b0abe5edfc0fbb59bc845bd65
Yurii Kalugin <pyro74rus@gmail.com>
2020-04-14 19:19:03 +0500

    Changes and migrations for admin panel authentication

--- users/api/backends.py
+++ users/api/backends.py
@@ -1,12 +1,14 @@
 from .requesters import authenticate_credentials
+from .models import User
 
 from generic.backends import GenericRemoteAuthBackend
 
 from django.conf import settings
-ERRORS_FIELD = getattr(settings, 'ERRORS_FIELD', 'error')
+ERRORS_FIELD = getattr(settings, 'ERRORS_FIELD', 'detail')
 
 
 class RemoteAuthBackend(GenericRemoteAuthBackend):
+    model = User
     ERRORS_FIELD = ERRORS_FIELD
 
     def authenticate_credentials(self, username, password):
