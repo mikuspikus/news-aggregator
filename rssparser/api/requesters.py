d41201a89ea1029b4247d78702195305fe767b60
Yurii Kalugin <pyro74rus@gmail.com>
2020-04-14 19:45:47 +0500

    Updated services for admin panel authentication

--- rssparser/api/requesters.py
+++ rssparser/api/requesters.py
@@ -8,9 +8,10 @@ from django.conf import settings
 from redis import StrictRedis
 
 __default_urls = {
+    'login' : 'http://localhost:8080/v0/users/login',
     'auth-token': 'http://localhost:8080/v0/tokens',
     'send-credentials': 'http://localhost:8080/v0/users',
-    'update-credentials' : 'http://localhost:8080/v0/users/{uuid}'
+    'update-credentials': 'http://localhost:8080/v0/users/{uuid}'
 }
 URLS = getattr(settings, 'URLS', __default_urls)
 
@@ -24,6 +25,20 @@ __default_redis_conf = {
     'port': 6379, 'db': 0, 'password': None, 'decode_responses': True}
 REDIS_CONF = getattr(settings, 'REDIS_CONF', __default_redis_conf)
 STORAGE = StrictRedis(**REDIS_CONF)
+
+
+def authenticate_credentials(username: str, password: str):
+    try:
+        response = base.post(
+            url=URLS['login'],
+            data={'username': username, 'password': password}
+        )
+
+    except RequestException as error:
+        return base.process_error(error)
+
+    return response.json(), response.status_code
+
 
 
 def authenticate(token: str) -> Tuple[dict, int]:
--- stats/api/requesters.py
+++ stats/api/requesters.py
@@ -1,17 +1,29 @@
 from generic import base
 from requests.exceptions import RequestException
 from typing import Tuple
+from redis import StrictRedis
 
 from django.conf import settings
 
 __default_urls = {
-    'authenticate': 'http://localhost:8080/v0/tokens',
-    'login': 'http://localhost:8080/v0/users/login',
+    'login' : 'http://localhost:8080/v0/users/login',
     'auth-token': 'http://localhost:8080/v0/tokens',
     'send-credentials': 'http://localhost:8080/v0/users',
-    'update-credentials' : 'http://localhost:8080/v0/users/{uuid}'
+    'update-credentials': 'http://localhost:8080/v0/users/{uuid}'
 }
 URLS = getattr(settings, 'URLS', __default_urls)
+
+ID = getattr(settings, 'APPID', 'users')
+SECRET = getattr(settings, 'APPSECRET', 'users-secret')
+
+TOKEN_LABEL = getattr(settings, 'TOKEN_LABEL', 'users-service-token')
+
+__default_redis_conf = {
+    'host': 'localhost',
+    'port': 6379, 'db': 0, 'password': None, 'decode_responses': True}
+REDIS_CONF = getattr(settings, 'REDIS_CONF', __default_redis_conf)
+STORAGE = StrictRedis(**REDIS_CONF)
+
 
 def authenticate_credentials(username: str, password: str):
     try:
