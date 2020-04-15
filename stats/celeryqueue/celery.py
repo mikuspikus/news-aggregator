b3cbc71e9f08cfcc14a7520fe3cc54baa1213e9a
Yurii Kalugin <pyro74rus@gmail.com>
2020-04-15 16:25:54 +0500

    Added console commands

--- stats/celeryqueue/celery.py
+++ stats/celeryqueue/celery.py
@@ -6,6 +6,7 @@ app = Celery(include = ['celeryqueue.tasks'])
 app.config_from_object(Config)
 
 # celery -A celeryqueue worker -l info
-
+# docker run -d -p 6379:6379 redis
+# service redis-server stop
 if __name__ == "__main__":
     app.start()
