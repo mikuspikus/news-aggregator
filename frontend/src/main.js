6da8957078efc799b24b03edc0109b15353e7c99
Yurii Kalugin <pyro74rus@gmail.com>
2020-04-24 22:39:01 +0500

    Initial commit for [frontend]

---
+++ frontend/src/main.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
---
+++ frontend/src/views/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id = "index">
+        <navbar/>
+        <feeds/>
+    </div>
+</template>
+
+<script>
+import navbar from '../components/utility/Navbar.vue'
+import Feeds from '../components/rssfeed/Feeds.vue'
+export default {
+    components: {
+        navbar, Feeds
+    }
+}
+</script>
