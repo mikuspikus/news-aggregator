9964fc30114419a2f2c54922823d667a51e25e35
Yurii Kalugin <pyro74rus@gmail.com>
2020-04-25 16:39:01 +0500

    Added grid layout

--- frontend/src/views/Index.vue
+++ frontend/src/views/Index.vue
@@ -1,16 +1,41 @@
 <template>
-    <div id = "index">
-        <navbar/>
-        <feeds/>
-    </div>
+  <div id="index">
+    <navbar />
+    <feeds />
+
+    <b-container fluid>
+      <b-row class="text-center">
+        <b-col></b-col>
+        <b-col cols="8">
+          <single-news />
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+
+      <b-row>
+        <b-col></b-col>
+        <b-col cols="8">
+          <comment />
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+    </b-container>
+
+  </div>
 </template>
 
 <script>
-import navbar from '../components/utility/Navbar.vue'
-import Feeds from '../components/rssfeed/Feeds.vue'
+import Navbar from "../components/utility/Navbar.vue";
+import Feeds from "../components/rssfeed/Feeds.vue";
+import Comment from '../components/comments/Comment.vue'
+import SingleNews from "../components/news/SingleNews.vue";
+
 export default {
-    components: {
-        navbar, Feeds
-    }
-}
+  components: {
+    Navbar,
+    Feeds,
+    Comment,
+    SingleNews
+  }
+};
 </script>
