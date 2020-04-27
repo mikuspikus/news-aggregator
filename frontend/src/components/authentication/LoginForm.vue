b03e11aeb58ef84f4266d63b09a798a8ba4f6abf
Yurii Kalugin <pyro74rus@gmail.com>
2020-04-27 19:28:49 +0500

    Added authentication components

---
+++ frontend/src/components/authentication/LoginForm.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="login-from">
+    <b-card
+      border-variant="dark"
+      header="Sign in"
+      header-text-variant="white"
+      header-tag="header"
+      header-bg-variant="dark"
+      header-class="text-center"
+    >
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <b-form-group id="input-group-usernmae" label="Username:" label-for="input-username">
+          <b-form-input
+            id="input-usernmae"
+            v-model="form.username"
+            type="text"
+            required
+            placeholder="Enter usernmae"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-pwd" label="Password:" label-for="input-pwd">
+          <b-form-input
+            id="input-pwd"
+            v-model="form.pwd"
+            required
+            type="password"
+            placeholder="Enter password"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-button type="submit" variant="outline-dark">Sign in</b-button>
+        <b-button type="reset" variant="dark">Reset</b-button>
+      </b-form>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginForm",
+
+  data() {
+    return {
+      show: true,
+      form: {
+        username: "",
+        pwd: ""
+      }
+    };
+  },
+
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+    },
+
+    onReset(event) {
+      event.preventDefault();
+
+      this.form.username = "";
+      this.form.pwd = "";
+
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    }
+  }
+};
+</script>
---
+++ frontend/src/components/authentication/RegisterForm.vue
@@ -0,0 +1,111 @@
+<template>
+  <div id="register-from">
+    <b-card
+      border-variant="dark"
+      header="Sign up"
+      header-text-variant="white"
+      header-tag="header"
+      header-bg-variant="dark"
+      header-class="text-center"
+    >
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <b-form-group id="input-group-usernmae" label="Your username:" label-for="input-username">
+          <b-form-input
+            id="input-usernmae"
+            v-model="form.username"
+            type="text"
+            required
+            placeholder="Enter username"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-email" label="Your email:" label-for="input-email">
+          <b-form-input
+            id="input-email"
+            v-model="form.email"
+            required
+            type="email"
+            placeholder="Enter email"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group id="input-group-pwd" label="Your password:" label-for="input-pwd">
+          <b-form-input
+            id="input-pwd"
+            v-model="form.pwd"
+            required
+            type="password"
+            placeholder="Enter password"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          id="input-group-pwd-confirmed"
+          label="Your password (confirmed):"
+          label-for="input-pwd-confirmed"
+        >
+          <b-form-input
+            id="input-pwd-confirmed"
+            v-model="form.pwdconf"
+            required
+            type="password"
+            placeholder="Enter password"
+          ></b-form-input>
+        </b-form-group>
+
+        <b-button type="submit" variant="outline-dark">Sign up</b-button>
+        <b-button type="reset" variant="dark">Reset</b-button>
+      </b-form>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RegisterForm",
+
+  data() {
+    return {
+      show: true,
+      form: {
+        username: "",
+        email: "",
+        pwd: "",
+        pwdconf: ""
+      }
+    };
+  },
+
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+
+      if (this.form.pwd !== this.form.pwdconf) {
+        this.$bvToast.toast(`Your passwords do not match`, {
+          title: "Error",
+          autoHideDelay: 5000,
+          toaster: "b-toaster-bottom-center"
+        });
+
+        return;
+      }
+
+      this.$router.go(-1);
+    },
+
+    onReset(event) {
+      event.preventDefault();
+
+      this.form.username = "";
+      this.form.pwd = "";
+      this.form.pwdconf = "";
+      this.form.email = "";
+      // Trick to reset/clear native browser form validation state
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    }
+  }
+};
+</script>
