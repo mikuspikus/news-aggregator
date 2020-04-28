61f8a4e10b8218b200a62ca18570b6401d0a2d94
Yurii Kalugin <pyro74rus@gmail.com>
2020-04-28 23:23:01 +0500

    Changed news field from URLField to UUIDField

--- comments/api/models.py
+++ comments/api/models.py
@@ -5,7 +5,7 @@ from uuid import uuid4
 class Comment(models.Model):
     # id = models.UUIDField(primary_key = True, default = uuid4)
     author = models.UUIDField()
-    news = models.URLField()
+    news = models.UUIDField()
     body = models.TextField(max_length=512)
     created = models.DateTimeField(auto_now_add=True)
     edited = models.DateTimeField(auto_now=True)
