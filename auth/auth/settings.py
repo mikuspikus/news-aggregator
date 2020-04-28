43dc01f457882af206347c417dca463415bc6c23
Yurii Kalugin <pyro74rus@gmail.com>
2020-04-28 15:47:25 +0500

    Added CORS for [auth]

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -37,13 +37,20 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'api',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:8060",
+    "http://127.0.0.1:9000"
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
