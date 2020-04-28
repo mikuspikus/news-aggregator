bd0de65276c80085ab18eab562a95131c7740302
Yurii Kalugin <pyro74rus@gmail.com>
2020-04-28 23:28:49 +0500

    Added fetchinf data to NewsSingleView

--- frontend/src/views/News.vue
+++ frontend/src/views/News.vue
@@ -5,7 +5,7 @@
       <b-row>
         <b-col />
         <b-col cols="8">
-          <news-single />
+          <news-single :news_uuid="news_uuid" />
         </b-col>
         <b-col />
       </b-row>
@@ -16,7 +16,11 @@
         <b-col />
         <b-col cols="8">
           <b-card class="text-left">
-            <comment-from />
+            <comment-form
+              :news_uuid="news_uuid"
+              :user_uuid="userUUID"
+              v-on:reload-comments="fetchComments()"
+            />
           </b-card>
         </b-col>
         <b-col />
@@ -24,18 +28,18 @@
 
       <b-row class="pt-3" />
 
-      <b-row>
+      <b-row v-for="comment in comments" :key="comment.id">
         <b-col />
         <b-col cols="8">
-          <comment />
-        </b-col>
-        <b-col />
-      </b-row>
-
-      <b-row>
-        <b-col />
-        <b-col cols="8">
-          <comment />
+          <comment
+            v-on:reload-comments="fetchComments()"
+            :id="comment.id"
+            :user_uuid="comment.author"
+            :news_uuid="news_uuid"
+            :body="comment.body"
+            :created="comment.created"
+            :edited="comment.edited"
+          />
         </b-col>
         <b-col />
       </b-row>
@@ -46,17 +50,51 @@
 <script>
 import Comment from "../components/comments/Comment.vue";
 import NewsSingle from "../components/news/NewsSingle.vue";
-import CommentFrom from "../components/comments/CommentForm.vue";
+import CommentForm from "../components/comments/CommentForm.vue";
 export default {
   name: "news-single-view",
 
   components: {
     Comment,
-    CommentFrom,
+    CommentForm,
     NewsSingle
   },
 
+  data() {
+    return {
+      news_uuid: this.$route.params.uuid,
+      comments: []
+    };
+  },
+
+  created() {
+    this.fetchComments();
+  },
+
+  methods: {
+    fetchComments() {
+      this.$httpcomment({
+        url: "comments",
+        method: "GET",
+        params: { news: this.uuid }
+      })
+        .then(response => {
+          this.comments = response.data;
+        })
+        .catch(error => {
+          this.$bvToast.toast(error.message, {
+            title: "Error",
+            autoHideDelay: 5000,
+            toaster: "b-toaster-bottom-center"
+          });
+        });
+    }
+  },
+
   computed: {
+    userUUID() {
+      return this.$store.getters.uuid;
+    },
     isLoggedIn() {
       return this.$store.getters.isLoggedIn;
     }
