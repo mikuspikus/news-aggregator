d2178f83964f63aac7a34d718426ce19b874bf56
Yurii Kalugin <pyro74rus@gmail.com>
2020-04-28 23:23:55 +0500

    Fixed wrong view to url mapping

--- comments/api/urls.py
+++ comments/api/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import CommentView, CommentsView
 
 urlpatterns = [
-    path(f"comments", CommentView.as_view(), name="comments"),
-    path(f"comments/<int:id_>", CommentsView.as_view(), name="comment"),
+    path(f"comments", CommentsView.as_view(), name="comments"),
+    path(f"comments/<int:id_>", CommentView.as_view(), name="comment"),
 ]
