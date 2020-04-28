e618eb83715d36ad6da43826c3830a62906024f5
Yurii Kalugin <pyro74rus@gmail.com>
2020-04-28 23:27:32 +0500

    Massive NewsSingle component update

--- frontend/src/components/news/NewsSingle.vue
+++ frontend/src/components/news/NewsSingle.vue
@@ -4,10 +4,10 @@
       <b-card-header header-border-variant="dark" header-bg-variant="light" class="m-0 p-0">
         <b-navbar type="light">
           <b-navbar-nav variant="light">
-            <b-navbar-brand class="ml-0 pl-0" href="#">{{ news.title }}</b-navbar-brand>
+            <b-navbar-brand class="ml-0 pl-0">{{ news.title }}</b-navbar-brand>
             <b-nav-item disabled variant="light">by</b-nav-item>
             <b-navbar-brand
-              :to="{ name: 'User', params: { uuid: news.author.uuid }}"
+              :to="{ name: 'User', params: { uuid: news.author.id }}"
               variant="light"
             >{{ news.author.username }}</b-navbar-brand>
           </b-navbar-nav>
@@ -40,18 +40,20 @@
 
       <b-card-body class="text-left">
         <template v-if="edit">
-          <news-form
-            v-bind:title.sync="news.title"
-            v-bind:url.sync="news.url"
-            v-bind:edit.sync="edit"
+          <news-edit-form
+            :news_uuid="news_uuid"
+            :user_uuid="userUUID"
+            :title.sync="news.title"
+            :url.sync="news.url"
+            :edited.sync="news.edited"
+            :edit.sync="edit"
+            v-on:reload-news="fetchData()"
           />
         </template>
 
         <template v-else>
           <b-card-text>
-            <b-link :href="news.url">
-            {{ news.url }}
-            </b-link>
+            <b-link :href="news.url">{{ news.url }}</b-link>
           </b-card-text>
         </template>
       </b-card-body>
@@ -63,48 +65,70 @@
 </template>
 
 <script>
-import NewsForm from "../news/NewsForm.vue";
+import NewsEditForm from "../news/NewsEditForm.vue";
 
 export default {
   components: {
-    NewsForm
+    NewsEditForm
   },
 
   props: {
-    ninfo: { type: Object, default: null }
+    news_uuid: { type: String }
   },
 
   data() {
     return {
       edit: false,
-      news: {
-        author: {uuid: '1', username: "news author"},
-        title: "news title",
-        url: "https://news.url",
-        created: "created date",
-        edited: "edited date"
-      }
+      news: { author: { id: null, username: null } }
     };
   },
 
   methods: {
+    fetchData() {
+      this.$httpnews({ url: `news/${this.news_uuid}`, method: "GET" })
+        .then(response => {
+          this.news = response.data;
+
+          this.$httpuser({ url: `users/${this.news.author}`, method: "GET" })
+            .then(response => {
+              this.news.author = response.data;
+            })
+            .catch(error => {
+              this.$bvToast.toast(error.message, {
+                title: "Error",
+                autoHideDelay: 5000,
+                toaster: "b-toaster-bottom-center"
+              });
+            });
+        })
+        .catch(error => {
+          this.$bvToast.toast(error.message, {
+            title: "Error",
+            autoHideDelay: 5000,
+            toaster: "b-toaster-bottom-center"
+          });
+        });
+    },
     editForm(event) {
       event.preventDefault();
       this.edit = !this.edit;
     }
   },
 
+  created() {
+    this.fetchData();
+  },
+
   computed: {
+    userUUID() {
+      return this.$store.getters.uuid;
+    },
     isLogged() {
       return this.$store.getters.isLoggedIn;
     },
 
     isOwner() {
-      return (
-        this.isLogged &&
-        this.ninfo !== null &&
-        this.$store.getters.username === this.ninfo.username
-      );
+      return this.isLogged && this.$store.getters.uuid === this.news.author.id;
     },
 
     footer() {
