d178badc5432cb3285be736646fe7f624230d358
Yurii Kalugin <pyro74rus@gmail.com>
2020-04-28 15:52:04 +0500

    Added news-form for authorized users

--- frontend/src/views/Index.vue
+++ frontend/src/views/Index.vue
@@ -1,6 +1,25 @@
 <template>
   <div id="index">
     <b-container fluid class="pt-5">
+      <b-row v-if="isLoggedIn">
+        <b-col />
+
+        <b-col cols="8" class="mb-5 text-left">
+          <b-card
+            border-variant="dark"
+            header="Post news"
+            header-text-variant="light"
+            header-tag="header"
+            header-bg-variant="dark"
+            header-class="text-center"
+            class="m-1 text-left"
+          >
+            <news-form />
+          </b-card>
+        </b-col>
+
+        <b-col />
+      </b-row>
       <template v-for="shortnews in news">
         <b-row :key="shortnews.uuid">
           <b-col />
@@ -21,11 +40,18 @@
 
 <script>
 import NewsShort from "../components/news/NewsShort.vue";
+import NewsForm from "../components/news/NewsForm.vue";
 
 export default {
-  name: "index",
+  name: "index-view",
 
-  components: { NewsShort },
+  components: { NewsShort, NewsForm },
+
+  computed:{
+    isLoggedIn() {
+      return this.$store.getters.isLoggedIn;
+    }
+  },
 
   data() {
     return {
@@ -50,6 +76,6 @@ export default {
         }
       ]
     };
-  },
+  }
 };
 </script>
