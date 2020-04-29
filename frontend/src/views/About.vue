920bb1ff91d9579e98661f2de6447d07f17a4557
Yurii Kalugin <pyro74rus@gmail.com>
2020-04-29 18:20:02 +0500

    Changed About view

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="not-found">
+    <b-container fluid>
+      <b-row class="text-left">
+        <b-col />
+        <b-col cols="8" class="pt-5 pb-5">
+          <b-jumbotron
+            bg-variant="dark"
+            text-variant="white"
+            border-variant="dark"
+            class="m-0"
+            header
+          >
+            <template v-slot:header>About</template>
+
+            <template v-slot:lead>Example implementation of news agragator with RSS-feed parser with Django 3, Django Rest Framework and Vue.js 2</template>
+          </b-jumbotron>
+        </b-col>
+        <b-col />
+      </b-row>
+    </b-container>
   </div>
 </template>
