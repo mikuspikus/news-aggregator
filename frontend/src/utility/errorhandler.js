5fa1feda94de8cb3b4e944bb7d9f58ad835391e9
Yurii Kalugin <pyro74rus@gmail.com>
2020-04-29 18:18:43 +0500

    Added generic error handling

---
+++ frontend/src/utility/errorhandler.js
@@ -0,0 +1,25 @@
+const ehandler = {
+    error(vue, error, eheader, nfreason) {
+        if (error.response) {
+            const response = error.response
+            switch (response.status) {
+                case 401:
+                    vue.$store.dispatch('logout');
+                    vue.$router.push({ name: 'Login' });
+                    break;
+
+                case 404:
+                    vue.$router.push({ name: 'NotFound', params: { reason: nfreason } });
+            }
+        }
+        else {
+            vue.$bvToast.toast(error.message, {
+                title: eheader,
+                autoHideDelay: 5000,
+                toaster: "b-toaster-bottom-center"
+            });
+        }
+    }
+}
+
+export default ehandler
