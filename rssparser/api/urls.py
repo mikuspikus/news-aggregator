f1d9ff9ed7f748e32448cb94bd53e44f56af3d4a
Yurii Kalugin <pyro74rus@gmail.com>
2020-04-29 17:57:24 +0500

    Fixed wrong view for parsed feeds url

--- rssparser/api/urls.py
+++ rssparser/api/urls.py
@@ -5,5 +5,5 @@ from .views import FeedView, FeedsView, FeedParserView
 urlpatterns = [
     path(f"feeds", FeedsView.as_view(), name="feeds"),
     path(f"feeds/<int:pk>", FeedView.as_view(), name="feed"),
-    path(f"feeds/<uuid:user>", FeedView.as_view(), name="feed-parse"),
+    path(f"feeds/<uuid:user>", FeedParserView.as_view(), name="feed-parse"),
 ]
