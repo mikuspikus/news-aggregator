1aeb94222c9ba43c593fa9867435d6716aae6dab
Yurii Kalugin <pyro74rus@gmail.com>
2020-05-01 18:36:31 +0500

    "OPTIONS" method added to SAFE_METHODS

--- news/api/permissions.py
+++ news/api/permissions.py
@@ -3,9 +3,10 @@ from rest_framework.views import Request
 from .models import News
 
 class IsAuthenticatedFor(IsAuthenticatedForMethods):
-    SAFE_METHODS = ('GET', )
+    SAFE_METHODS = ('GET', 'OPTIONS')
 
 class IsAuthorizedAndNewsOwner(IsAuthorizedAndOwner):
+    SAFE_METHODS = ('GET', 'OPTIONS')
 
     def is_owner(self, news: News, request: Request) -> bool:
         return str(news.author) == request.auth.get('uuid')
