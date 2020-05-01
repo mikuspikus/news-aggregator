bd89cea0458b84d71f582e2ce1a29307aaff3e9b
Yurii Kalugin <pyro74rus@gmail.com>
2020-05-01 13:23:38 +0500

    Added GET and OPTIONS to  SAFE_METHODS

--- comments/remoteauth/permissions.py
+++ comments/remoteauth/permissions.py
@@ -3,6 +3,7 @@ from rest_framework.views import Request, APIView
 
 
 class IsRemoteAuthenticated(BasePermission):
+    SAFE_METHODS = ['OPTIONS', "GET"]
 
     def has_permission(self, request: Request, view: APIView) -> bool:
-        return bool(request.auth)
+        return request.method in self.SAFE_METHODS or bool(request.auth)
