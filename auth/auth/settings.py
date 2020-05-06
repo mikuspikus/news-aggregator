70d6e178b5e8cb0d2943c3614e37b5182615046b
Yurii Kalugin <pyro74rus@gmail.com>
2020-05-06 21:05:28 +0500

    Added custom ligon page to [auth] for OAuth2 support

--- auth/auth/settings.py
+++ auth/auth/settings.py
@@ -14,13 +14,15 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
+STATIC_DIR = os.path.join(BASE_DIR, "static")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%rog+^zae(h94-)1%((49%g5#h&g50j#stoxwkma&j-_oa2vpy'
+SECRET_KEY = "%rog+^zae(h94-)1%((49%g5#h&g50j#stoxwkma&j-_oa2vpy"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -31,64 +33,66 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'corsheaders',
-    'oauth2_provider',
-    'rest_framework',
-    'api',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "corsheaders",
+    "oauth2_provider",
+    "rest_framework",
+    "api",
 ]
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:8060",
     "http://localhost:8086",
-    "http://127.0.0.1:9000"
+    "http://127.0.0.1:9000",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'auth.urls'
+ROOT_URLCONF = "auth.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [TEMPLATES_DIR],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'auth.wsgi.application'
+STATICFILES_DIRS = [STATIC_DIR]
 
-AUTH_USER_MODEL = 'api.AuthUser'
+WSGI_APPLICATION = "auth.wsgi.application"
+
+AUTH_USER_MODEL = "api.AuthUser"
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -98,26 +102,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -129,15 +127,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 
 TOKEN_EXPIRED_AFTER_SECONDS = 3600
 
 CREDENTIALS = {
-    'AUTH' : {'ID' : 'auth', 'SECRET' : 'auth-secret'},
-    'USERS' : {'ID' : 'users', 'SECRET' : 'users-secret'},
-    'COMMENTS' : {'ID' : 'comments', 'SECRET' : 'comments-secret'},
-    'RSS-PARSER' : {'ID' : 'rss-parser', 'SECRET' : 'rss-parser-secret'},
-    'NEWS' : {'ID' : 'news', 'SECRET' : 'news-secret'},
-    'STATS' : {'ID' : 'stats', 'SECRET' : 'stats-secret'},
+    "AUTH": {"ID": "auth", "SECRET": "auth-secret"},
+    "USERS": {"ID": "users", "SECRET": "users-secret"},
+    "COMMENTS": {"ID": "comments", "SECRET": "comments-secret"},
+    "RSS-PARSER": {"ID": "rss-parser", "SECRET": "rss-parser-secret"},
+    "NEWS": {"ID": "news", "SECRET": "news-secret"},
+    "STATS": {"ID": "stats", "SECRET": "stats-secret"},
 }
--- auth/auth/urls.py
+++ auth/auth/urls.py
@@ -17,7 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('v0/', include('api.urls')),
-    path('oauth2/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    path("admin/", admin.site.urls),
+    path("v0/", include("api.urls")),
+    path("oauth2/", include("oauth2_provider.urls", namespace="oauth2_provider")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
