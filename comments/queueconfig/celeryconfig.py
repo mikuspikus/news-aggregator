bfbb62fecbd1630f32886f438855750253277bb6
Yurii Kalugin <pyro74rus@gmail.com>
2020-05-25 18:54:31 +0500

    Added timeout to send_task

--- comments/queueconfig/celeryconfig.py
+++ comments/queueconfig/celeryconfig.py
@@ -10,3 +10,5 @@ DB = os.environ.get('R_DB', '0')
 class Config:
     accept_content = {'json'}
     broker_url = f"redis://{PASSWORD}{HOST}:{PORT}/{DB}"
+    task_soft_time_limit=30,
+    task_time_limit=120,
